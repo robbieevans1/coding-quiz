060b3f1830eb547967db0bf07c7ec4992717d6e0
robcabobb <robcabobb@gmail.com>
2021-12-24 09:08:10 -0500

    finished js for quiz end

---
+++ js/end.js
@@ -0,0 +1,34 @@
+const initials = document.querySelector('#initials')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = document.querySelector('#mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.innerText = mostRecentScore
+
+initials.addEventListener('keyup', () => {
+     saveScoreBtn.disabled = !initials.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: initials.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+}
